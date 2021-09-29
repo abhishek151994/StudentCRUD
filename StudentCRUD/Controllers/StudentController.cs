37095d66b6218b02c88df11e27125866ebd655ad
Abhishek Patil <abhishek.patil@ecotechservices.com>
2021-09-29 14:22:27 +0530

    Add project files.

---
+++ StudentCRUD/Controllers/StudentController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentCRUD.Models;
+namespace StudentCRUD.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        studentDBEntities dBEntities = new studentDBEntities();
+        public ActionResult Index()
+        {
+            //StudentModel studentModel = new StudentModel();
+
+
+
+            return View(dBEntities.students.ToList());
+        }
+
+        // GET: Student/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: /Student/Create  
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //  
+        // POST: /Student/Create  
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(student stud)
+        {
+            if (ModelState.IsValid)
+            {
+                dBEntities.students.Add(stud);
+                dBEntities.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(stud);
+        }
+
+        // GET: /Student/Edit/5  
+        [HttpGet]
+        public ActionResult Edit(int rollno)
+        {
+
+            student stud = dBEntities.students.SingleOrDefault(x => x.RollNo == rollno);
+            if (stud == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stud);
+        }
+
+        //  
+        // POST: /Student/Edit/5  
+
+        [HttpPost]
+
+        public ActionResult Edit(student s)
+        {
+            //Edit([Bind(Exclude   ="RollNo")] student s)
+            //Edit([Bind(Include   ="Name,Address,Mobile,Email")] student s)//Property binding
+            // s.Name = dBEntities.students.Single(x => x.RollNo == s.RollNo).Name;
+
+           // student st = dBEntities.students.Single(x=>x.RollNo==rn);
+            //UpdateModel<Istudent>(st); //Model binding using interface
+       
+            if (ModelState.IsValid)
+            {
+                dBEntities.Entry(s).State = EntityState.Modified;
+                dBEntities.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(s);
+        }
+
+        public ActionResult Details1(int rollno)
+        {
+            student stud = dBEntities.students.Single(x=>x.RollNo==rollno);
+            //ViewBag.student = stud;
+            return View(stud);
+        }
+
+
+        // POST: Student/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int rollno)
+        {
+            studentDBEntities dBEntities = new studentDBEntities();
+
+            student stud = dBEntities.students.Find(rollno);
+
+            dBEntities.students.Remove(stud);
+            dBEntities.SaveChanges();
+
+            return RedirectToAction("Index");
+
+
+
+        } 
+    }
+}
---
+++ StudentCRUD/Models/student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace StudentCRUD.Models
+{
+  public  interface Istudent
+    {
+         string Name { get; set; }
+         string Address { get; set; }
+         string Mobile { get; set; }
+         string Email { get; set; }
+
+
+    }
+    public class student:Istudent
+    {
+        [Key]
+        [Required(ErrorMessage ="Roll No Is Required")]
+         public int RollNo { get; set; }
+        [Required]
+        [StringLength(15, MinimumLength = 4, ErrorMessage = "Name length between 4 to 15")]
+       // [DisplayName("Full Name")]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(15, MinimumLength = 4, ErrorMessage = "Address length between 4 to 15")]
+       // [ScaffoldColumn(false)]
+       //[DisplayFormat(NullDisplayText ="Address Not Specified")]
+       [ReadOnly(true)]
+        public string Address { get; set; }
+        [Required]
+        [RegularExpression(@"\+91[0-9]{10}", ErrorMessage = "Mobile Invalid")]
+        public string Mobile { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    
+
+    }
+}
